2c06e41e46ef42d8e71ec9ed58f1543f95d787cf
Wenjun Kang <wkang2@bsd.uchicago.edu>
2022-05-21 18:07:09 -0500

    Changed ugettext_lazy to gettext_lazy for Django 4.0 compatibility

--- audit_log/models/__init__.py
+++ audit_log/models/__init__.py
@@ -1,5 +1,5 @@
 from django.db.models import Model
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from audit_log.models.fields import CreatingUserField, CreatingSessionKeyField, LastUserField, LastSessionKeyField
 
 class AuthStampedModel(Model):
