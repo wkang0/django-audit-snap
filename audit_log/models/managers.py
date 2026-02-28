c98c4af027fa101301b104ebbf33020c5c0909dd
wkang2 <wkang2@bsd.uchicago.edu>
2026-02-27 21:11:58 -0600

    Refactor code for improved readability and consistency in managers.py

--- audit_log/models/managers.py
+++ audit_log/models/managers.py
@@ -12,9 +12,11 @@ from audit_log import settings as local_settings
 
 try:
     from django.utils.timezone import now as datetime_now
+
     assert datetime_now
 except ImportError:
     import datetime
+
     datetime_now = datetime.datetime.now
 
 
@@ -23,41 +25,54 @@ class LogEntryObjectDescriptor(object):
         self.model = model
 
     def __get__(self, instance, owner):
-        kwargs = dict((f.attname, getattr(instance, f.attname))
-                      for f in self.model._meta.fields
-                      if hasattr(instance, f.attname))
+        kwargs = dict(
+            (f.attname, getattr(instance, f.attname))
+            for f in self.model._meta.fields
+            if hasattr(instance, f.attname)
+        )
         return self.model(**kwargs)
 
 
 class AuditLogManager(models.Manager):
-    def __init__(self, model, attname, instance=None, ):
+    def __init__(
+        self,
+        model,
+        attname,
+        instance=None,
+    ):
         super(AuditLogManager, self).__init__()
         self.model = model
         self.instance = instance
         self.attname = attname
         # set a hidden attribute on the  instance to control wether we should track changes
-        if instance is not None and not hasattr(instance, '__is_%s_enabled' % attname):
-            setattr(instance, '__is_%s_enabled' % attname, True)
+        if instance is not None and not hasattr(instance, "__is_%s_enabled" % attname):
+            setattr(instance, "__is_%s_enabled" % attname, True)
 
     def enable_tracking(self):
         if self.instance is None:
-            raise ValueError("Tracking can only be enabled or disabled "
-                             "per model instance, not on a model class")
-        setattr(self.instance, '__is_%s_enabled' % self.attname, True)
+            raise ValueError(
+                "Tracking can only be enabled or disabled "
+                "per model instance, not on a model class"
+            )
+        setattr(self.instance, "__is_%s_enabled" % self.attname, True)
 
     def disable_tracking(self):
         if self.instance is None:
-            raise ValueError("Tracking can only be enabled or disabled "
-                             "per model instance, not on a model class")
-        setattr(self.instance, '__is_%s_enabled' % self.attname, False)
+            raise ValueError(
+                "Tracking can only be enabled or disabled "
+                "per model instance, not on a model class"
+            )
+        setattr(self.instance, "__is_%s_enabled" % self.attname, False)
 
     def is_tracking_enabled(self):
         if local_settings.DISABLE_AUDIT_LOG:
             return False
         if self.instance is None:
-            raise ValueError("Tracking can only be enabled or disabled "
-                             "per model instance, not on a model class")
-        return getattr(self.instance, '__is_%s_enabled' % self.attname)
+            raise ValueError(
+                "Tracking can only be enabled or disabled "
+                "per model instance, not on a model class"
+            )
+        return getattr(self.instance, "__is_%s_enabled" % self.attname)
 
     def get_queryset(self):
         if self.instance is None:
@@ -101,12 +116,12 @@ class AuditLog(object):
     def post_save(self, instance, created, **kwargs):
         # ignore if it is disabled
         if getattr(instance, self.manager_name).is_tracking_enabled():
-            self.create_log_entry(instance, created and 'I' or 'U')
+            self.create_log_entry(instance, created and "I" or "U")
 
     def post_delete(self, instance, **kwargs):
         # ignore if it is disabled
         if getattr(instance, self.manager_name).is_tracking_enabled():
-            self.create_log_entry(instance, 'D')
+            self.create_log_entry(instance, "D")
 
     def finalize(self, sender, **kwargs):
         log_entry_model = self.create_log_entry_model(sender)
@@ -114,7 +129,9 @@ class AuditLog(object):
         models.signals.post_save.connect(self.post_save, sender=sender, weak=False)
         models.signals.post_delete.connect(self.post_delete, sender=sender, weak=False)
 
-        descriptor = AuditLogDescriptor(log_entry_model, self.manager_class, self.manager_name)
+        descriptor = AuditLogDescriptor(
+            log_entry_model, self.manager_class, self.manager_name
+        )
         setattr(sender, self.manager_name, descriptor)
 
     def copy_fields(self, model):
@@ -123,7 +140,7 @@ class AuditLog(object):
         track of for the provided model, returning a
         dictionary mapping field name to a copied field object.
         """
-        fields = {'__module__': model.__module__}
+        fields = {"__module__": model.__module__}
 
         for field in model._meta.fields:
 
@@ -158,11 +175,15 @@ class AuditLog(object):
                     field.db_index = True
 
                 if field.remote_field and field.remote_field.related_name:
-                    field.remote_field.related_name = '_auditlog_%s' % field.remote_field.related_name
+                    field.remote_field.related_name = (
+                        "_auditlog_%s" % field.remote_field.related_name
+                    )
                 elif field.remote_field:
                     try:
                         if field.remote_field.get_accessor_name():
-                            field.remote_field.related_name = '_auditlog_%s' % field.remote_field.get_accessor_name()
+                            field.remote_field.related_name = (
+                                "_auditlog_%s" % field.remote_field.get_accessor_name()
+                            )
                     except:
                         pass
 
@@ -175,41 +196,51 @@ class AuditLog(object):
         Returns a dictionary mapping of the fields that are used for
         keeping the acutal audit log entries.
         """
-        rel_name = '_%s_audit_log_entry' % model._meta.object_name.lower()
+        rel_name = "_%s_audit_log_entry" % model._meta.object_name.lower()
 
         def entry_instance_to_unicode(log_entry):
             try:
-                result = '%s: %s %s at %s' % (model._meta.object_name,
-                                              log_entry.object_state,
-                                              log_entry.get_action_type_display().lower(),
-                                              log_entry.action_date,
-
-                                              )
+                result = "%s: %s %s at %s" % (
+                    model._meta.object_name,
+                    log_entry.object_state,
+                    log_entry.get_action_type_display().lower(),
+                    log_entry.action_date,
+                )
             except AttributeError:
-                result = '%s %s at %s' % (model._meta.object_name,
-                                          log_entry.get_action_type_display().lower(),
-                                          log_entry.action_date
-
-                                          )
+                result = "%s %s at %s" % (
+                    model._meta.object_name,
+                    log_entry.get_action_type_display().lower(),
+                    log_entry.action_date,
+                )
             return result
 
         action_user_field = LastUserField(related_name=rel_name, editable=False)
 
         # check if the manager has been attached to auth user model
-        if [model._meta.app_label, model.__name__] == getattr(settings, 'AUTH_USER_MODEL', 'auth.User').split("."):
-            action_user_field = LastUserfield(related_name=rel_name, editable=False, to='self')
+        if [model._meta.app_label, model.__name__] == getattr(
+            settings, "AUTH_USER_MODEL", "auth.User"
+        ).split("."):
+            action_user_field = LastUserField(
+                related_name=rel_name, editable=False, to="self"
+            )
 
         return {
-            'action_id': models.AutoField(primary_key=True),
-            'action_date': models.DateTimeField(default=datetime_now, editable=False, blank=False),
-            'action_user': action_user_field,
-            'action_type': models.CharField(max_length=1, editable=False, choices=(
-                ('I', _('Created')),
-                ('U', _('Changed')),
-                ('D', _('Deleted')),
-            )),
-            'object_state': LogEntryObjectDescriptor(model),
-            '__unicode__': entry_instance_to_unicode,
+            "action_id": models.AutoField(primary_key=True),
+            "action_date": models.DateTimeField(
+                default=datetime_now, editable=False, blank=False
+            ),
+            "action_user": action_user_field,
+            "action_type": models.CharField(
+                max_length=1,
+                editable=False,
+                choices=(
+                    ("I", _("Created")),
+                    ("U", _("Changed")),
+                    ("D", _("Deleted")),
+                ),
+            ),
+            "object_state": LogEntryObjectDescriptor(model),
+            "__unicode__": entry_instance_to_unicode,
         }
 
     def get_meta_options(self, model):
@@ -218,12 +249,13 @@ class AuditLog(object):
         autdit log model.
         """
         result = {
-            'ordering': ('-action_date',),
-            'app_label': model._meta.app_label,
+            "ordering": ("-action_date",),
+            "app_label": model._meta.app_label,
         }
         from django.db.models.options import DEFAULT_NAMES
-        if 'default_permissions' in DEFAULT_NAMES:
-            result.update({'default_permissions': ()})
+
+        if "default_permissions" in DEFAULT_NAMES:
+            result.update({"default_permissions": ()})
         return result
 
     def create_log_entry_model(self, model):
@@ -234,6 +266,6 @@ class AuditLog(object):
 
         attrs = self.copy_fields(model)
         attrs.update(self.get_logging_fields(model))
-        attrs.update(Meta=type(str('Meta'), (), self.get_meta_options(model)))
-        name = str('%sAuditLogEntry' % model._meta.object_name)
+        attrs.update(Meta=type(str("Meta"), (), self.get_meta_options(model)))
+        name = str("%sAuditLogEntry" % model._meta.object_name)
         return type(name, (models.Model,), attrs)
